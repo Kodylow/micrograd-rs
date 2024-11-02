392f2ffff6dba7a2ab356731ebdc9034e855f20e
kody.low <kodylow7@gmail.com>
2024-11-02 16:15:49 -0700

    impl other operators for value

--- src/nn.rs
+++ src/nn.rs
@@ -18,6 +18,33 @@ impl std::ops::Add for Value {
     }
 }
 
+// Implement operator overloading for *
+impl std::ops::Mul for Value {
+    type Output = Value;
+
+    fn mul(self, rhs: Value) -> Value {
+        Value::new(self.data * rhs.data)
+    }
+}
+
+// Implement operator overloading for -
+impl std::ops::Sub for Value {
+    type Output = Value;
+
+    fn sub(self, rhs: Value) -> Value {
+        Value::new(self.data - rhs.data)
+    }
+}
+
+// Implement operator overloading for /
+impl std::ops::Div for Value {
+    type Output = Value;
+
+    fn div(self, rhs: Value) -> Value {
+        Value::new(self.data / rhs.data)
+    }
+}
+
 // Implement Display trait for pretty printing
 impl std::fmt::Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
